070edb51c1395ff7920da71cf040542166ba2836
Rajesh M <rajeshm@rajeshs-mac.local>
2016-11-29 00:03:52 +0530

    commit-swift3 with pods framework 'Spring'

--- FirstApp9/ViewController.swift
+++ FirstApp9/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var layer1: SpringView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        
+        layer1.animation = "squeezeDown"
+        layer1.animate()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirstApp9' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+ use_frameworks!
+ pod 'Spring', :git => 'https://github.com/MengTo/Spring.git', :branch => 'swift3'
+
+  target 'FirstApp9Tests' do
+    inherit! :search_paths
+   use_frameworks!
+pod 'Spring', :git => 'https://github.com/MengTo/Spring.git', :branch => 'swift3'
+  end
+
+  target 'FirstApp9UITests' do
+    inherit! :search_paths
+    use_frameworks!
+pod 'Spring', :git => 'https://github.com/MengTo/Spring.git', :branch => 'swift3'
+  end
+
+end
