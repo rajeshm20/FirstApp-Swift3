a01d47e3fe3e7032302e810dbf08694c7760f5dc
Rajesh M <rajeshm@rajeshs-mac.local>
2016-12-08 23:30:46 +0530

    commit-pod eureka

--- Pods/Target Support Files/Pods-FirstApp9UITests/Pods-FirstApp9UITests-frameworks.sh
+++ Pods/Target Support Files/Pods-FirstApp9UITests/Pods-FirstApp9UITests-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Spring/Spring.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Eureka/Eureka.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Spring/Spring.framework"
 fi
